e83809e02808de587cd25bf8faa3a2bb58a1ed88
Gabriel Ribeiro Domingues <gabrielrd123@gmail.com>
2023-11-13 17:11:43 -0300

    correção commit errado

--- src/Array.java
+++ src/Array.java
@@ -60,25 +60,35 @@ public class Array {
             }
         }
     }
-}
-    /*
+
     public  void insertionSort(int array[]) {
         int x = array.length;
         for (int j = 1; j < x; j++) {
-            int array2 = array[j];
+            int chave = array[j];
             int i = j - 1;
-            while ((i > -1) && (array[i] > array2)) {
+            while ((i >= 0) && (array[i] > chave)) {
                 array[i + 1] = array[i];
                 i--;
             }
-            array[i + 1] = array2;
-            //printArray(array);
+            array[i + 1] = chave;
+            printArray(array);
         }
+        System.out.println();
         System.out.println("Após a aplicação do Insertion Sort");    
         for(int i:array){    
-            System.out.print(i+ " ");    
-        }
+            System.out.print(i+ " ");
+        }  
     }
-    */
+    
+    public void printArray(int array[]){
+        int n = array.length;
+        System.out.println("Array atual: ");
+        for (int i = 0; i < n; i++){
+            System.out.println(array[i] + " ");
+        }
+        System.out.println();
+
+    }
+}
 
 
